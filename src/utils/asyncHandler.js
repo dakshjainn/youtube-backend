17218c45365208edb23e83f9a2f4cc156576d571
Daksh <dakshjainn2004@gmail.com>
2024-07-08 19:24:46 +0530

    asyncHandler

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+const asyncHandler = (fn) => {
+    (req, res, next) => {
+        // Promise.resolve(fn(req, res, next)).catch(next);
+        Promise.resolve(fn(req, res, next)).catch((error) => next(error)) // this is same as above line, explicitly specifies the error parameter in the .catch block and passes it to next;
+    }
+}
+
+
+
+/* using try catch
+
+// const asyncHandler = () => {}
+// const asyncHandler = (fn) => {() => {}}
+// const asyncHandler = (fn) => async{() => {}}
+// const asyncHandler = (fn) => async () => {}
+
+const asyncHandler = (fn) => async(req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false, 
+            message: error.message || "Something went wrong"
+        })
+    }
+}
+*/
